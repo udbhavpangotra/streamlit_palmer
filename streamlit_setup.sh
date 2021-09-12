17452770a05a272e2a0d2c367246cf07bdd06e02
Udbhav Pangotra <udbhavpangotra@ymail.com>
2021-09-13 00:29:24 +0530

    Update streamlit_setup.sh

--- streamlit_setup.sh
+++ streamlit_setup.sh
@@ -1,6 +1,13 @@
-mkdir -p ~/.streamlit
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+
+echo "\
+    [general]\n\
+    email = \"udbhavpangotra23@gmail.com\"\n\
+    " > ~/.streamlit/credentials.toml
+
+echo "\
+    [server]\n\
+    headless = true\n\
+    enableCORS = false
+    port = $PORT\n\
+    " > ~/.streamlit/config.toml
